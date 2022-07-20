eb21f03b8bc13a70a4a7b47a104cd8b53a09eee3
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2022-07-20 11:50:26 +0200

    Volume bind for local.config.php

--- omeka-s/docker-entrypoint.sh
+++ omeka-s/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Omeka configuration that needs to happen during runtime
+### Omeka configurations that need to happen during runtime ###
 # Note: Docker volume-binds are not available during build stage.
 mkdir /var/www/html/files/temp
 chown www-data:www-data /var/www/html/volume/files/temp
+### End of Omeka configurations ###
 
 # End with a persistent foreground process
 apache2-foreground
