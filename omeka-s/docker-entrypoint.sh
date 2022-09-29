c3632dd0c711c9df8691372b01fd2e9c34e897ef
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2022-09-29 10:32:42 +0200

    Fully working Omeka Shibboleth SSO. Manual steps have been automated in Dockerfile and docker-entrypoint.sh

--- omeka-s/docker-entrypoint.sh
+++ omeka-s/docker-entrypoint.sh
@@ -10,8 +10,15 @@ fi
 
 ### End of Omeka configurations ###
 
+# Start the Shibboleth daemon for single-sign on
+service shibd start
+
 # Start the Apache daemon
 service apache2 start
 
+# Download the IdP signing certificate
+curl -L ${SAML_IDP_SIGN_CERT_URL} --output /etc/shibboleth/idp-sign-cert.pem
+
 # End with a persistent foreground process
+echo "--- Omeka S startup DONE ---"
 tail -F /var/www/html/logs/application.log
