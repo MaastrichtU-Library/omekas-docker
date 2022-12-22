6cf9bcbc8c877c2a6413131ce155b0c54ac98893
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2022-12-22 12:35:10 +0100

    Working version of local ARK resolver and rewrite rule

--- omeka-s/local-ark-site-resolver.php
+++ omeka-s/local-ark-site-resolver.php
@@ -1,31 +1,40 @@
 <?php
 
-# You need to add the following to your Apache configuration (exception to the rule not to server php files):
-# RewriteCond %{REQUEST_FILENAME} -f
-# RewriteRule ark-site-resolver.php$ - [NC,C]
-# RewriteRule .* - [L]
+# Local ARK resolver
 
-# RewriteRule ^ark:/([0-9]+)/([a-zA-Z0-9]+)$ https://www.goudatijdmachine.nl/data/custom/ark-site-resolver.php?ark=ark:/$1/$2 [R=301,L]
+# Developer: Maarten Coonen, Maastricht University Library, 2022.
+# Adapted from the code developed by Gouda Tijd Machine.
+# Many thanks to the Bob Coret, developer at Gouda Tijd Machine & Netwerk Digital Erfgoed, for sharing the code with us.
 
 
-# https://n2t.net/ark:/60537/b9MTov 
-# [302] > https://www.goudatijdmachine.nl/ark:/60537/b9MTov
-# [302] > https://www.goudatijdmachine.nl/data/custom/ark-site-resolver.php?ark=ark:/60537/b9MTov
+# You need to add a rewrite rule like below to your Apache configuration or .htaccess file to forward incoming ARKs to this local resolver code:
+# RewriteRule ^ark:/99999/([a-zA-Z0-9]+)$ local-ark-site-resolver.php?ark=ark:/99999/$1 [PT,L]
 
-# ARK resolver to right site
+# This local resolver code works like this:
+# 1. Global resolver https://n2t.net/ark:/99999/a12vpho redirects to [302] > https://<BASE URL of your NAAN registration>/ark:/99999/a12vpho
+# 2. RewriteRule redirects to ./local-ark-site-resolver.php?ark=ark:/99999/a12vpho
+# 3. The local resolver redirects to [302] > https://<BASE URL of your NAAN registration>/<$omeka_basepath>/<$slug>/ark:/99999/a12vpho
 
-$omeka_basepath="/data/";
+# Example:
+# 0. https://n2t.net/ark:/99999/a12vpho
+# 1. https://omeka.local/ark:/99999/a12vpho
+# 2. https://omeka.local/local-ark-site-resolver.php?ark=ark:/99999/a12vpho
+# 3. https://omeka.local/s/examplesite/ark:/99999/a12vpho
+
+
+// Given the URL http://omeka.local/s/examplesite/ark:/99999/a12vpho
+// the $omeka_basepath would be "/s/"
+// and the lookup result for $slug would be 'examplesite'
+$omeka_basepath="/s/";
 
 if (isset($_GET["ark"])) {
 	#$ark="ark:/60537/b9MTov";
 	$ark=preg_replace('/[^a-z0-9\:\/]/i','',$_GET["ark"]);
 
 	# get omeka database settings
-
-	$database_settings=parse_ini_file("database.ini");  # symlinked
+	$database_settings=parse_ini_file("config/database.ini");
 
 	# connect to db via mysqli
-
 	try {
 		$mysqli = new mysqli($database_settings["host"], $database_settings["user"], $database_settings["password"], $database_settings["dbname"]);
 	} catch (\mysqli_sql_exception $e) {
@@ -33,9 +42,8 @@ if (isset($_GET["ark"])) {
 	}
 
 	# find site slug
-
-	$stmt = $mysqli->prepare("SELECT slug, v.resource_id, r.resource_type FROM omeka.value v LEFT JOIN omeka.item_site i ON v.resource_id=i.item_id LEFT JOIN omeka.site s ON i.site_id=s.id LEFT JOIN omeka.resource r ON v.resource_id=r.id WHERE v.value=?");
-	#$stmt = $mysqli->prepare("SELECT slug FROM omeka.value v, omeka.item_site i, omeka.site s WHERE v.value=? AND v.resource_id=i.item_id AND i.site_id=s.id");
+	$dbname = $database_settings["dbname"];	// Note: $dbname can be left out in query below
+	$stmt = $mysqli->prepare("SELECT slug, v.resource_id, r.resource_type FROM $dbname.value v LEFT JOIN $dbname.item_site i ON v.resource_id=i.item_id LEFT JOIN $dbname.site s ON i.site_id=s.id LEFT JOIN $dbname.resource r ON v.resource_id=r.id WHERE v.value=?");
 	$stmt->bind_param("s", $ark);
 	$stmt->execute();
 	$result = $stmt->get_result()->fetch_assoc();
@@ -55,19 +63,19 @@ if (isset($_GET["ark"])) {
 
 		if ($result["resource_type"]=="Omeka\Entity\ItemSet") {
 		    if (empty($slug)) {
-				header("Location: ".$omeka_basepath."collection/".$ark);
+				header("Location: " . $omeka_basepath . "collection/" . $ark);
 			} else {
-				header("Location: ".$omeka_basepath.$slug."/collection/".$ark);
+				header("Location: " . $omeka_basepath . $slug . "/collection/" . $ark);
 			}
 			exit;
 		} else {
 			$slug=$result["slug"];
-			header("Location: ".$omeka_basepath.$slug."/document/".$ark);
+			header("Location: " . $omeka_basepath . $slug . "/" . $ark);
 			exit;
 		}
 	}
 
-	header("Location: ".$omeka_basepath);
+	header("Location: " . $omeka_basepath);
 
 } else {
 	echo "<h1>Missing ARK</h1>";
