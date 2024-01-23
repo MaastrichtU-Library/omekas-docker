0c00bf4220495f24f70cbfa576568530536e2431
Peter Verberne <p.verberne@maastrichtuniversity.nl>
2024-01-23 11:36:48 +0100

    Waiting for solr to alter schema - trying 5 times.

--- solr/docker-entrypoint.sh
+++ solr/docker-entrypoint.sh
@@ -10,12 +10,12 @@ precreate-core omekas
 solr-foreground &
 
 # Change the Solr Schema on intial start only
-sleep 5
+sleep 10
 if ! [ -f /var/solr/solr_schema_modified ]; then
     echo "Modifying Solr schema via API..."
     # Copy all to '_text_' field for cases where there is no default field described.
     # See: https://gitlab.com/Daniel-KM/Omeka-S-module-SearchSolr/-/blob/master/README.md#fixing-the-issue-when-there-is-no-result
-    curl -X POST --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}' 'http://localhost:8983/solr/omekas/schema'
+    curl --retry 5 --connect-timeout 5 -X POST --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}' 'http://localhost:8983/solr/omekas/schema'
     touch /var/solr/solr_schema_modified
     echo "Done"
 fi
