069aa0c15d955dd1f9fcca143b8f0ce979494202
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2024-01-23 09:59:00 +0100

    Solr schema must only be modified at initial run

--- solr/docker-entrypoint.sh
+++ solr/docker-entrypoint.sh
@@ -9,12 +9,17 @@ precreate-core omekas
 # Start Solr in foreground and continue with the rest of this script 
 solr-foreground &
 
+# Change the Solr Schema on intial start only
 sleep 5
-echo "Modifying Solr schema via API..."
-# Copy all to '_text_' field for cases where there is no default field described.
-# See: https://gitlab.com/Daniel-KM/Omeka-S-module-SearchSolr/-/blob/master/README.md#fixing-the-issue-when-there-is-no-result
-curl -X POST --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}' 'http://localhost:8983/solr/omekas/schema'
-echo "Done"
+if ! [ -f /var/solr/solr_schema_modified ]; then
+    echo "Modifying Solr schema via API..."
+    # Copy all to '_text_' field for cases where there is no default field described.
+    # See: https://gitlab.com/Daniel-KM/Omeka-S-module-SearchSolr/-/blob/master/README.md#fixing-the-issue-when-there-is-no-result
+    curl -X POST --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}' 'http://localhost:8983/solr/omekas/schema'
+    touch /var/solr/solr_schema_modified
+    echo "Done"
+fi
+
 
 # Return to the foreground process (writes to stdout)
 fg %1
