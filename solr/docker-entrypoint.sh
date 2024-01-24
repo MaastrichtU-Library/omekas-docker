78999b8dbaa61f1d59c91315ca763f71b56fb1a1
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2024-01-24 17:43:21 +0100

    Made Solr core name an environment setting

--- solr/docker-entrypoint.sh
+++ solr/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 set -m
 
 # Pre-create the Solr core
-precreate-core omekas
+precreate-core ${ENV_SOLR_CORE}
 
 # Start Solr in foreground and continue with the rest of this script 
 solr-foreground &
@@ -20,10 +20,10 @@ if ! [ -f /var/solr/solr_schema_modified ]; then
     echo "Modifying Solr schema via API..."
     # Copy all to '_text_' field for cases where there is no default field described.
     # See: https://gitlab.com/Daniel-KM/Omeka-S-module-SearchSolr/-/blob/master/README.md#fixing-the-issue-when-there-is-no-result
-    curl --retry 5 --connect-timeout 5 -X POST --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}' 'http://localhost:8983/solr/omekas/schema'
+    curl --retry 5 --connect-timeout 5 -X POST http://localhost:8983/solr/${ENV_SOLR_CORE}/schema --data-binary '{"add-copy-field":{"source":"*","dest":"_text_" }}'
 
     # Add the 'solr.ASCIIFoldingFilterFactory' class to the index- and query-analyzers
-    curl --retry 5 --connect-timeout 5 -X POST -H 'Content-type:application/json' http://localhost:8983/solr/omekas/schema --data-binary @- << EOF
+    curl --retry 5 --connect-timeout 5 -X POST -H 'Content-type:application/json' http://localhost:8983/solr/${ENV_SOLR_CORE}/schema --data-binary @- << EOF
 {
 	"replace-field-type": {
 		"name": "text_general",
